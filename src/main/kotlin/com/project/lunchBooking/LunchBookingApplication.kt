07c1d41c89cca4cd03533c49ce89115963c26eda
joe061005 <18233368@life.hkbu.edu.hk>
2022-08-07 19:15:47 +0800

    Initial commit and add user API

---
+++ src/main/kotlin/com/project/lunchBooking/LunchBookingApplication.kt
@@ -0,0 +1,11 @@
+package com.project.lunchBooking
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LunchBookingApplication
+
+fun main(args: Array<String>) {
+	runApplication<LunchBookingApplication>(*args)
+}
