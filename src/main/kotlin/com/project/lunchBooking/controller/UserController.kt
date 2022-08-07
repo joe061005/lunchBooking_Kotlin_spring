2c6bc92a3624692f79b34f10b79054e45511ccd5
joe061005 <18233368@life.hkbu.edu.hk>
2022-08-07 22:01:24 +0800

    edit user API components

--- src/main/kotlin/com/project/lunchBooking/controller/UserController.kt
+++ src/main/kotlin/com/project/lunchBooking/controller/UserController.kt
@@ -1,12 +1,13 @@
 package com.project.lunchBooking.controller
 
+import com.project.lunchBooking.repo.UserRepository
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("test")
-class Test {
+@RequestMapping("api")
+class UserController(private val userRepository: UserRepository) {
 
     @GetMapping
     fun helloWorld(): String{
--- src/main/kotlin/com/project/lunchBooking/repo/UserRepository.kt
+++ src/main/kotlin/com/project/lunchBooking/repo/UserRepository.kt
@@ -3,5 +3,6 @@ package com.project.lunchBooking.repo
 import com.project.lunchBooking.model.User
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserRepository: JpaRepository<User, Long> {
+interface UserRepository: JpaRepository<User, Int> {
+    
 }
